1ce7073c3dc49e24e5a7bade4f57c90a14361adc
Ustinov Tikhon <tikhon.ustinov@south.rt.ru>
2020-09-20 21:59:34 +0300

    Добавил обработку ошибки

--- modules/botCommand/messageAll.js
+++ modules/botCommand/messageAll.js
@@ -4,11 +4,23 @@ module.exports = async (ctx) => {
     let message = ctx.update.message.text.replace('/message', '')
         .replace('@oss_bss_wc_bot', '')
     if (chat.username === process.env.ADMIN_USERNAME && message.length) {
+        let success = []
+        let errors = []
         let chats = await db.Chat.getChats()
         for (let chatId of chats) {
-            await ctx.telegram.sendMessage(chatId, message)
+            try {
+                await ctx.telegram.sendMessage(chatId, message)
+                success.push(chatId)
+            } catch (err) {
+                console.log(err)
+                errors.push(`${chatId}: ${err.message}`)
+            }
         }
-        await ctx.reply(`Отправлено в ${chats.length} чатов`)
+        let result = 'Отправлено'
+        result += `\nУспешно: ${success.length}`
+        result += `\nОшибки: ${errors.length}`
+        result += errors.join('\n\n')
+        await ctx.reply(result)
     }
 
 }
