823f7a6383e04b4d3237e9193f62cb93c52a8812
Ustinov Tikhon <tikhon.ustinov@south.rt.ru>
2020-09-20 18:12:04 +0300

    Теперь если пользователь запускает не Job не в своем Pipeline то уведомление приходит и тому кто запустил pipeline и тому кто перезапустил Job

--- modules/util.js
+++ modules/util.js
@@ -1,53 +1,59 @@
 function getStatus(status) {
-  let text = ''
-  //, pending, , , , skipped, created
-  switch (status) {
-    case 'running':
-      text = '▶'
-      break
-    case 'canceled':
-      text = '❕'
-      break
-    case 'success':
-      text = '✅'
-      break
-    case 'failed':
-      text = '❗'
-      break
-  }
-  return text
+    let text = ''
+    //, pending, , , , skipped, created
+    switch (status) {
+        case 'running':
+            text = '▶'
+            break
+        case 'canceled':
+            text = '❕'
+            break
+        case 'success':
+            text = '✅'
+            break
+        case 'failed':
+            text = '❗'
+            break
+    }
+    return text
 }
-async function createPipelineResponse (webhookInfo) {
-  let text = ''
-  if (webhookInfo.object_kind === 'pipeline' ) {
+
+async function createPipelineResponse(webhookInfo) {
+    let text = ''
+    webhookInfo.builds.sort((a, b) => {
+      if (a.id > b.id) return -1
+      if (a.id < b.id) return 1
+      return 0
+    })
     let db = global.DB
-    let userName = await db.Alias.getTelegramName(webhookInfo.user.username)
+    let gitLabUser = webhookInfo.builds[0].user.username
+    let userName = '@' + (await db.Alias.getTelegramName(gitLabUser) || gitLabUser)
+    if (gitLabUser !== webhookInfo.user.username) {
+        userName += ', @' + (await db.Alias.getTelegramName(webhookInfo.user.username) || webhookInfo.user.username)
+    }
     let attr = webhookInfo.object_attributes
     let status = getStatus(attr.status)
     if (status.length) {
-      text = `Status: ${status} (${attr.status})
+        text = `Status: ${status} (${attr.status})
 Project: ${webhookInfo.project.name}
 Branch: ${attr.ref}
-User: @${userName || webhookInfo.user.username}
+User(s): ${userName}
 Commit message: ${webhookInfo.commit.message}
 URL Pipeline: ${webhookInfo.project.web_url}/pipelines/${attr.id}`
-      if (attr.status === 'failed') {
-        let errors = webhookInfo.builds.filter(build => {
-          return build.status === 'failed'
-        })
-        if (errors.length) {
-          for (let err of errors) {
-            text += `\nJob: ${err.name}`
-            text += `\nError url: ${webhookInfo.project.web_url}/-/jobs/${err.id}`
-          }
+        if (attr.status === 'failed') {
+            let errors = webhookInfo.builds.filter(build => build.status === 'failed')
+            if (errors.length) {
+                for (let err of errors) {
+                    text += `\nJob: ${err.name}`
+                    text += `\nError url: ${webhookInfo.project.web_url}/-/jobs/${err.id}`
+                }
+            }
         }
-      }
     }
-  }
-  return text
+    return text
 }
 
 module.exports = {
-  getStatus,
-  createPipelineResponse
+    getStatus,
+    createPipelineResponse
 }
--- routes/webhook.js
+++ routes/webhook.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const store = require('../modules/store')
 const Telegram = require('telegraf/telegram')
 const telegram = new Telegram(process.env.BOT_TOKEN)
 const util = require('../modules/util')
@@ -8,56 +7,60 @@ const util = require('../modules/util')
 const fakeCash = new Set()
 
 /* GET users listing. */
-router.post('/', async function(req, res) {
-  let message = await util.createPipelineResponse(req.body)
-  let db = global.DB
-  let project = req.body.project
-  let attr = req.body.object_attributes
-  if (!message.length || fakeCash.has(message + project)) {
-    return res.send('ok')
-  }
-  fakeCash.add(message + project)
-  setTimeout(() => {
-    fakeCash.delete(message + project)
-  }, 60000)
-  let db_project = await db.Project.get(project.name, project.namespace)
-  let db_branch = await db.Branch.getByProject(attr.ref, db_project)
-  let db_chats_id = await db.ChatProject.getChatsId(db_project)
-  let chatsIds = []
-  for (let id of db_chats_id) {
-    let chat = await db.Chat.getChatById(id)
-    if (chat) {
-      chatsIds.push({
-        id: chat.telegramChatId,
-        stepsIncluded: chat.stepsIncluded
-      })
+router.post('/', async function (req, res, next) {
+    if (req.body.object_kind !== 'pipeline' || req.body.merge_request) {
+        next()
+        return
     }
-  }
-  let db_subscribes = await db.Subscribe.getAllByBranchId(db_branch.id)
-  for (let subscribe of db_subscribes) {
-    let chat = await db.Chat.getChatById(subscribe.chatId)
-    if (chat) {
-      let index = chatsIds.findIndex(c => c.id === chat.id)
-      if (index) {
-        chatsIds.push({
-          id: chat.telegramChatId,
-          stepsIncluded: chat.stepsIncluded
-        })
-      }
+    let message = await util.createPipelineResponse(req.body)
+    let db = global.DB
+    let project = req.body.project
+    let attr = req.body.object_attributes
+    if (!message.length || fakeCash.has(message + project)) {
+        return res.send('ok')
     }
-  }
-  for (let chat of chatsIds) {
-    try {
-      await telegram.sendMessage(chat.id, message)
-    } catch (e) {
-      if (e.code === 400) {
-        await db.Chat.removeChatById(chat.id)
-      } else {
-        console.log(e.message)
-      }
+    fakeCash.add(message + project)
+    setTimeout(() => {
+        fakeCash.delete(message + project)
+    }, 2 * 60 * 100)
+    let db_project = await db.Project.get(project.name, project.namespace)
+    let db_branch = await db.Branch.getByProject(attr.ref, db_project)
+    let db_chats_id = await db.ChatProject.getChatsId(db_project)
+    let chatsIds = []
+    for (let id of db_chats_id) {
+        let chat = await db.Chat.getChatById(id)
+        if (chat) {
+            chatsIds.push({
+                id: chat.telegramChatId,
+                stepsIncluded: chat.stepsIncluded
+            })
+        }
     }
-  }
-  res.send('ok');
+    let db_subscribes = await db.Subscribe.getAllByBranchId(db_branch.id)
+    for (let subscribe of db_subscribes) {
+        let chat = await db.Chat.getChatById(subscribe.chatId)
+        if (chat) {
+            let index = chatsIds.findIndex(c => c.id === chat.id)
+            if (index) {
+                chatsIds.push({
+                    id: chat.telegramChatId,
+                    stepsIncluded: chat.stepsIncluded
+                })
+            }
+        }
+    }
+    for (let chat of chatsIds) {
+        try {
+            await telegram.sendMessage(chat.id, message)
+        } catch (e) {
+            if (e.code === 400) {
+                await db.Chat.removeChatById(chat.id)
+            } else {
+                console.log(e.message)
+            }
+        }
+    }
+    res.send('ok');
 });
 
 module.exports = router;
